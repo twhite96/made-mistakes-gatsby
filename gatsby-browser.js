cb07cd632ae42d5fc9277d6dbb3b4a7217e40e2e
Michael Rose <est.michael@gmail.com>
2019-09-18 16:40:36 -0400

    Add IntersectionObserver polyfill for gatsby-image

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,9 @@
 // prismjs color theme
 require('./src/styles/prism.css')
+
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+  }
+}
