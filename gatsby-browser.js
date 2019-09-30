00c32672feebf57de570c6cf70192fcd0a2a1463
Michael Rose <est.michael@gmail.com>
2019-09-30 16:19:07 -0400

    Add custom block styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
-// prismjs color theme
-require('./src/styles/prism.css')
+require('./src/styles/custom-block.css')
+require('./src/styles/prism.css') // prismjs color theme
 
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-image (Safari, IE)
