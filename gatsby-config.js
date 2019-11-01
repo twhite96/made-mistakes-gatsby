58cc50764194c1b15b71150507c7f763cbb62eca
Michael Rose <est.michael@gmail.com>
2019-11-01 14:25:34 -0400

    Reduce image quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -208,7 +208,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 800,
-              quality: 80,
+              quality: 75,
               linkImagesToOriginal: false,
             },
           },
