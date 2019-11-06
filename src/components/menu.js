8ebf772775845891543999dd2fe1198851da0aed
Michael Rose <est.michael@gmail.com>
2019-11-06 11:00:23 -0500

    Remove icon

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import Icon from './Icon'
 
 import style from '../styles/Menu.module.css'
 
@@ -18,10 +17,6 @@ const MainMenu = ({ mainMenu }) => {
 
   return <ul className={style.primaryMenu}>{items}</ul>
 }
-
-const toggleIcon = `M22 41C32.4934 41 41 32.4934 41 22C41 11.5066 32.4934 3 22
-3C11.5066 3 3 11.5066 3 22C3 32.4934 11.5066 41 22 41ZM7 22C7
-13.7157 13.7157 7 22 7V37C13.7157 37 7 30.2843 7 22Z`
 
 const Menu = ({
   mainMenu,
@@ -59,7 +54,9 @@ const Menu = ({
           type="button"
           aria-label="Theme toggle"
         >
-          <Icon style={{ cursor: 'pointer' }} size={24} d={toggleIcon} />
+          <div className={style.themeToggleInner}>
+            <div className={style.themeToggleIcon} />
+          </div>
         </button>
       </div>
     </>
