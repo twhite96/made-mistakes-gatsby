e96b63cb72851faa64c89b612320564673a84250
Michael Rose <est.michael@gmail.com>
2019-11-12 16:36:30 -0500

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,72 +18,74 @@ const HomePage = ({ data }) => (
       metaImage={site.image}
     />
     <main className={style.main}>
-      <div className={style.title}>
-        <h1 className={style.heading} style={{ display: 'none' }}>
-          {site.title}
-        </h1>
-      </div>
+      <h1>
+        Made Mistakes is the personal site of <a href="/about/">Michael Rose</a>
+        .
+      </h1>
       <div className={style.content}>
-        <Section
-          title="A faux pas"
-          description="<p>Hello, my name is Michael Rose.</p><p>I'm just another
-        boring, tattooed, <em>time traveling designer</em> from Buffalo New York.</p>
-        <p>I enjoy eating chicken wings, sketching on an iPad Pro, building
-        with static site generators, and playing Nintendo Switch.</p>"
-        >
-          <Img
-            fixed={data.aboutImage.childImageSharp.fixed}
-            style={{
-              maxWidth: `100%`,
-            }}
-            imgStyle={{
-              marginBottom: `0`,
-            }}
-          />
+        <div className={style.intro}>
           <p>
-            <Link to="/about/">Learn more about me</Link>
+            I'm just another boring, tattooed, >time traveling designer from
+            Buffalo New York who enjoys eating chicken wings, sketching on an
+            iPad Pro, building with static site generators, and playing Nintendo
+            Switch.
           </p>
-        </Section>
-        <Section
-          title="Writing"
-          description="<p>A collection of thoughts, inspiration, mistakes, and other
-        minutia I've written. Topics covered include
-        <a href='/tag/web-development/'><em>web development</em></a>,
-        <a href='/mastering-paper/'><em>Mastering Paper</em></a>,
-        <a href='/tag/design/'><em>design</em></a>, <a href='/tag/til/'><em>#TIL</em></a>,
-        and more <a href='/tag/' title='browse all site topics'>more</a>.</p>"
-        >
-          <Img
-            fixed={data.writingImage.childImageSharp.fixed}
-            style={{
-              maxWidth: `100%`,
-            }}
-            imgStyle={{
-              marginBottom: `0`,
-            }}
-          />
-          <p>
-            <Link to="/articles/">Browse the archives</Link>
-          </p>
-        </Section>
-        <Section
-          title="Work"
-          description="<p>A hand-picked selection of things I've designed, illustrated,
-        and developed.</p>"
-        >
-          <Img
-            fixed={data.worksImage.childImageSharp.fixed}
-            style={{
-              maxWidth: `100%`,
-            }}
-            imgStyle={{
-              marginBottom: `0`,
-            }}
-          />
-          <p>
-            <Link to="/work/">Give them a look</Link>
-          </p>
-        </Section>
+        </div>
+        <h2>
+          Popular articles [<a href="/articles/">browse all</a>]
+        </h2>
+        <div>
+          <h2>Explore more on this site</h2>
+          <ul>
+            <li>
+              <a href="/articles/">Articles</a>
+              <p>Long form writing mostly about web development.</p>
+            </li>
+            <li>
+              <a href="/notes/">Notes</a>
+              <p>
+                Thoughts, inspiration, mistakes, and other minutia I've written.
+              </p>
+            </li>
+            <li>
+              <a href="/mastering-paper/">Mastering Paper</a>
+              <p>
+                Tutorials to help master the iOS drawing app,{' '}
+                <strong>Paper</strong>.
+              </p>
+            </li>
+            <li>
+              <a href="/about/">About</a>
+              <p>More about me than you care to know.</p>
+            </li>
+            <li>
+              <a href="/contact/">Contact</a>
+              <p>
+                Preferred methods of sending your questions, messages, and love
+                letters to me.
+              </p>
+            </li>
+            <li>
+              <a href="/work/">Works</a>
+              <p>
+                Hand-picked selection of things I've designed, illustrated, and
+                developed.
+              </p>
+            </li>
+            <li>
+              <a href="/support/">Show your support</a>
+              <p>Show thanks for the free open source goodies I provide.</p>
+            </li>
+            <li>
+              <a href="/faqs/">Frequently asked questions</a>
+              <p>There's no such thing as a...</p>
+            </li>
+            <li>
+              <a href="/tag/">All topics</a>
+              <p>Archive of all posts organized by topic.</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   </Layout>
