a8d97b0e279cbea859680b7748565e9ff0780259
Michael Rose <est.michael@gmail.com>
2019-11-15 22:51:59 -0500

    Add smooth scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,15 @@ export const onClientEntry = () => {
   if (typeof window.IntersectionObserver === `undefined`) {
     import(`intersection-observer`)
   }
+
+  if (typeof window !== 'undefined') {
+    // eslint-disable-next-line global-require
+    require('smooth-scroll')('a[href*="#"]', {
+      speed: 600,
+      speedAsDuration: true,
+      easing: 'easeInOutQuad',
+    })
+  }
 }
 
 export const onServiceWorkerUpdateReady = () => {
