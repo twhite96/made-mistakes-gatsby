787bc5f6f80a0efb2512f9271a2c9340eeba85e9
Michael Rose <est.michael@gmail.com>
2019-12-05 16:30:48 -0500

    Rip out inifnite scroll

--- src/pages/paperfaces.js
+++ src/pages/paperfaces.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import chunk from 'lodash/chunk'
 import Masonry from 'react-masonry-component'
 import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
@@ -12,133 +11,72 @@ import style from '../styles/Document.module.css'
 
 const metaImage = site.image
 
-// This would normally be in a Redux store or some other global data store.
-if (typeof window !== `undefined`) {
-  window.postsToShow = 32
-}
+const GalleryPage = ({ data }) => {
+  const {
+    site: {
+      siteMetadata: { author: siteAuthor },
+    },
+    allMarkdownRemark: { edges: posts },
+  } = data
+  return (
+    <Layout>
+      <SEO
+        title={`PaperFaces iPad project - ${site.title}`}
+        path="/paperfaces/"
+        description="PaperFaces portraits painted digitally with Paper for
+        iOS on an iPad. Find time lapse videos, in-process screenshots, and more."
+        metaImage={metaImage}
+      />
+      <main className={style.document}>
+        <div className={style.title}>
+          <h1 className={style.heading}>
+            <span>PaperFaces iPad project</span>
+          </h1>
+        </div>
+        {data.file.childImageSharp.fluid && (
+          <Img
+            fluid={data.file.childImageSharp.fluid}
+            className={style.cover}
+          />
+        )}
+        <div className={style.content}>
+          <p>
+            PaperFaces was an illustration project by designer Michael Rose —
+            hey that’s me! For two years I drew the faces of strangers everyday
+            using an iPad, a stylus, and <strong>Paper for iOS</strong>. I
+            occasionally post new portraits here, but certainly not as
+            frequently as I used to.
+          </p>
+          <p>
+            If you scroll down far enough you can see how my technique evolved
+            from faceless gestures into realistic portraits.
+          </p>
+        </div>
+        <div className={style.gallery}>
+          {posts.map(({ node }) => {
+            const {
+              id,
+              frontmatter: { path, image, thumbnail },
+            } = node
+            const newImage = thumbnail ? thumbnail : image
 
-class Gallery extends React.Component {
-  constructor() {
-    super()
-    const postsToShow = 32
-
-    this.state = {
-      showingMore: postsToShow > 32,
-      postsToShow,
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener(`scroll`, this.handleScroll)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(`scroll`, this.handleScroll)
-    window.postsToShow = this.state.postsToShow
-  }
-
-  handleScroll = () => {
-    if (!this.ticking) {
-      this.ticking = true
-      requestAnimationFrame(() => this.update())
-    }
-  }
-
-  update() {
-    const distanceToBottom =
-      document.documentElement.offsetHeight -
-      (window.scrollY + window.innerHeight)
-    if (this.state.showingMore && distanceToBottom < 100) {
-      this.setState(prevState => ({ postsToShow: prevState.postsToShow + 32 }))
-    }
-    this.ticking = false
-  }
-
-  render() {
-    const posts = this.props.data.allMarkdownRemark.edges.map(e => e.node)
-
-    return (
-      <Layout>
-        <SEO
-          title={`PaperFaces iPad project - ${site.title}`}
-          path="/paperfaces/"
-          description="PaperFaces portraits painted digitally with Paper for
-          iOS on an iPad. Find time lapse videos, in-process screenshots, and more."
-          metaImage={metaImage}
-        />
-        <main className={style.document}>
-          <div className={style.title}>
-            <h1 className={style.heading}>
-              <span>PaperFaces iPad project</span>
-            </h1>
-          </div>
-          {this.props.data.file.childImageSharp.fluid && (
-            <Img
-              fluid={this.props.data.file.childImageSharp.fluid}
-              className={style.cover}
-            />
-          )}
-          <div className={style.content}>
-            <p>
-              PaperFaces was an illustration project by designer Michael Rose —
-              hey that’s me! For two years I drew the faces of strangers
-              everyday using an iPad, a stylus, and{' '}
-              <strong>Paper for iOS</strong>. I occasionally post new portraits
-              here, but certainly not as frequently as I used to.
-            </p>
-            <p>
-              If you scroll down far enough you can see how my technique evolved
-              from faceless gestures into realistic portraits.
-            </p>
-          </div>
-          <div className={style.gallery}>
-            {chunk(
-              posts.slice(0, this.state.postsToShow),
-              this.state.postsToShow
-              // eslint-disable-next-line no-shadow
-            ).map((chunk, i) => (
-              <Masonry className={style.grid} key={`chunk-${i}`}>
-                {chunk.map(post => {
-                  const image = post.frontmatter.thumbnail
-                    ? post.frontmatter.thumbnail
-                    : post.frontmatter.image
-
-                  return (
-                    <div key={post.id} className={style.gridItem}>
-                      <Link to={post.frontmatter.path}>
-                        <Img
-                          fadeIn={false}
-                          fluid={image.childImageSharp.fluid}
-                        />
-                      </Link>
-                    </div>
-                  )
-                })}
+            return (
+              <Masonry className={style.grid} key={id}>
+                <div key={id} className={style.gridItem}>
+                  <Link to={path}>
+                    <Img fadeIn={false} fluid={image.childImageSharp.fluid} />
+                  </Link>
+                </div>
               </Masonry>
-            ))}
-            {!this.state.showingMore && (
-              <button
-                type="button"
-                data-testid="load-more"
-                className={style.loadMore}
-                onClick={() => {
-                  this.setState(prevState => ({
-                    postsToShow: prevState.postsToShow + 32,
-                    showingMore: true,
-                  }))
-                }}
-              >
-                Load more
-              </button>
-            )}
-          </div>
-        </main>
-      </Layout>
-    )
-  }
+            )
+          })}
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
-Gallery.propTypes = {
+GalleryPage.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
@@ -204,4 +142,4 @@ export const pageQuery = graphql`
   }
 `
 
-export default Gallery
+export default GalleryPage
