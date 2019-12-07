48b246382502d0d1eef2b801a909e964562e6b95
Michael Rose <est.michael@gmail.com>
2019-12-06 19:59:33 -0500

    Fix case

---
+++ src/components/Entry.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import { format } from 'date-fns'
+
+import style from '../styles/Entry.module.css'
+
+const _ = require('lodash-addons')
+
+const Entry = ({
+  title,
+  date,
+  path,
+  image,
+  author,
+  timeToRead,
+  excerpt,
+  tags,
+}) => {
+  return (
+    <>
+      <article className={`${style.entry} h-entry`}>
+        {image && (
+          <Img fluid={image.childImageSharp.fluid} className={style.cover} />
+        )}
+        <h2 className={`${style.title} p-name`}>
+          <Link to={path}>{title}</Link>
+        </h2>
+        <div className={style.meta}>
+          {author && (
+            <span style={{ display: 'none' }}>
+              Published by{' '}
+              <a className="p-author h-card" href={author.url}>
+                {author.name}
+              </a>
+            </span>
+          )}
+          {date && (
+            <>
+              {' '}
+              <time className={`${style.date} dt-published`} dateTime={date}>
+                {format(new Date(date), 'MMMM eo, yyyy')}
+              </time>
+            </>
+          )}
+          {timeToRead && (
+            <>
+              {' '}
+              <span className={style.readTime}>
+                {timeToRead}&nbsp;min&nbsp;read
+              </span>
+            </>
+          )}
+          {tags ? (
+            <div className={style.tags}>
+              {tags.map(tag => (
+                <Link to={`/tag/${_.slugify(tag)}/`} key={_.slugify(tag)}>
+                  <span>#{tag}</span>
+                </Link>
+              ))}
+            </div>
+          ) : null}
+        </div>
+
+        <div
+          className={`${style.excerpt} p-summary`}
+          dangerouslySetInnerHTML={{ __html: excerpt }}
+        />
+      </article>
+    </>
+  )
+}
+
+Entry.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  path: PropTypes.string,
+  image: PropTypes.object,
+  author: PropTypes.object,
+  timeToRead: PropTypes.number,
+  excerpt: PropTypes.string,
+  tags: PropTypes.array,
+}
+
+export default Entry
---
+++ src/components/SEO/index.js
@@ -0,0 +1,223 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
+import useSiteMetadata from '../useSiteMetadata'
+import OpenGraph from './OpenGraph'
+import TwitterCard from './TwitterCard'
+
+function SEO({
+  title,
+  description,
+  metaImage,
+  path,
+  article,
+  datePublished,
+  dateModified,
+  showRecaptcha,
+}) {
+  const {
+    buildTime,
+    siteMetadata: {
+      siteUrl,
+      defaultTitle,
+      defaultDescription,
+      defaultBanner,
+      siteLanguage,
+      ogLanguage,
+      pingbackUrl,
+      webmentionUrl,
+      micropubUrl,
+      coilUrl,
+      author,
+      twitter,
+      facebook,
+    },
+  } = useSiteMetadata()
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${metaImage.src || defaultBanner}`,
+    url: `${siteUrl}${path || ''}`,
+  }
+
+  // schema.org in JSONLD format
+  // https://developers.google.com/search/docs/guides/intro-structured-data
+
+  const schemaOrgWebPage = {
+    '@context': 'http://schema.org',
+    '@type': 'WebPage',
+    url: siteUrl,
+    inLanguage: siteLanguage,
+    mainEntityOfPage: siteUrl,
+    description: defaultDescription,
+    name: defaultTitle,
+    author: {
+      '@type': 'Person',
+      name: author.name,
+    },
+    copyrightHolder: {
+      '@type': 'Person',
+      name: author.name,
+    },
+    creator: {
+      '@type': 'Person',
+      name: author.name,
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: defaultTitle,
+      logo: {
+        '@type': 'ImageObject',
+        url: `${siteUrl}${defaultBanner}`,
+      },
+    },
+    datePublished,
+    dateModified: buildTime,
+    image: {
+      '@type': 'ImageObject',
+      url: seo.image,
+    },
+  }
+
+  let schemaArticle = null
+
+  if (article) {
+    schemaArticle = {
+      '@context': 'http://schema.org',
+      '@type': 'Article',
+      author: {
+        '@type': 'Person',
+        name: author.name,
+      },
+      creator: {
+        '@type': 'Person',
+        name: author.name,
+      },
+      publisher: {
+        '@type': 'Organization',
+        name: defaultTitle,
+        logo: {
+          '@type': 'ImageObject',
+          url: `${siteUrl}${defaultBanner}`,
+        },
+      },
+      headline: seo.title,
+      datePublished,
+      dateModified,
+      description: seo.description,
+      inLanguage: siteLanguage,
+      url: seo.url,
+      name: seo.title,
+      image: {
+        '@type': 'ImageObject',
+        url: seo.image,
+      },
+      mainEntityOfPage: seo.url,
+    }
+  }
+
+  return (
+    <>
+      <Helmet title={seo.title}>
+        <html lang={siteLanguage} />
+        <link rel="canonical" href={seo.url} />
+        {pingbackUrl && <link rel="pingback" href={pingbackUrl} />}
+        {webmentionUrl && <link rel="webmention" href={webmentionUrl} />}
+        {micropubUrl && <link rel="micropub" href={micropubUrl} />}
+        {coilUrl && <meta name="monetization" content={coilUrl} />}
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
+        {/* Insert schema.org data conditionally (webpage/article) */}
+        {!article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaOrgWebPage)}
+          </script>
+        )}
+        {article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaArticle)}
+          </script>
+        )}
+        {showRecaptcha && (
+          <script
+            src="https://www.google.com/recaptcha/api.js?&render=explicit"
+            async
+            defer
+          />
+        )}
+      </Helmet>
+      <OpenGraph
+        description={seo.description}
+        image={seo.image}
+        title={seo.title}
+        type={article ? 'article' : 'website'}
+        url={seo.url}
+        locale={ogLanguage}
+        name={facebook}
+      />
+      <TwitterCard
+        title={seo.title}
+        image={seo.image}
+        description={seo.description}
+        username={twitter}
+      />
+    </>
+  )
+}
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  datePublished: PropTypes.string,
+  dateModified: PropTypes.string,
+  metaImage: PropTypes.shape({
+    src: PropTypes.string,
+    height: PropTypes.number,
+    width: PropTypes.number,
+  }),
+  path: PropTypes.string,
+  article: PropTypes.bool,
+  showRecaptcha: PropTypes.bool,
+}
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  datePublished: null,
+  dateModified: null,
+  metaImage: null,
+  path: null,
+  article: false,
+  showRecaptcha: false,
+}
+
+// const query = graphql`
+//   query SEO {
+//     site {
+//       buildTime(formatString: "YYYY-MM-DD")
+//       siteMetadata {
+//         siteUrl
+//         defaultTitle: title
+//         defaultDescription: description
+//         defaultBanner: image {
+//           src
+//         }
+//         siteLanguage
+//         ogLanguage
+//         pingbackUrl
+//         webmentionUrl
+//         micropubUrl
+//         coilUrl
+//         author {
+//           name
+//           url
+//         }
+//         twitter
+//         facebook
+//       }
+//     }
+//   }
+// `
+
+export default SEO
