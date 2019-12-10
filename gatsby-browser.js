39f55e93d30b8eded36d4dce2258b14bb130f3aa
Michael Rose <est.michael@gmail.com>
2019-12-10 13:23:25 -0500

    Remove smooth-scroll plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,13 +8,4 @@ export const onClientEntry = () => {
   if (typeof window.IntersectionObserver === `undefined`) {
     import(`intersection-observer`)
   }
-
-  if (typeof window !== 'undefined') {
-    // eslint-disable-next-line global-require
-    require('smooth-scroll')('a[href*="#"]', {
-      speed: 600,
-      speedAsDuration: true,
-      easing: 'easeInOutQuad',
-    })
-  }
 }
