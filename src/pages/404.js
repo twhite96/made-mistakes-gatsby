a8accbe0981d17da0b19ef5679e399f5c221fc3a
Michael Rose <est.michael@gmail.com>
2019-12-31 14:33:30 -0500

    Fix 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import style from '../styles/archive.module.css'
+
 import site from '../../config/site'
 
 const NotFoundPage = () => (
@@ -11,8 +13,16 @@ const NotFoundPage = () => (
       description="Sorry, but the pixels you are looking for are in another castle."
       metaImage={site.image}
     />
-    <h1>404: Not found</h1>
-    <p>Sorry, but the pixels you are looking for are in another castle.</p>
+    <main id="main" className={style.main}>
+      <div className={style.title}>
+        <h1 className={style.heading}>
+          <span>404: Not found</span>
+        </h1>
+      </div>
+      <div className={style.content}>
+        <p>Sorry, but the pixels you are looking for are in another castle.</p>
+      </div>
+    </main>
   </Layout>
 )
 
